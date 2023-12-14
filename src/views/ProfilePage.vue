b5449bc4656b60f1625503b8027f0f8202783f3b
lilpipiman <lminecraft369@gmail.com>
2023-12-14 12:29:01 +0100

    Profil

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script>
 
 </script>
 
@@ -6,7 +6,7 @@
   <v-container>
     <v-row>
       <v-col>
-        <h1 class="ueberschrift">Profil</h1>
+        <h1 class="ueberschrift">User123456</h1>
       </v-col>
     </v-row>
     <v-row>
@@ -16,17 +16,29 @@
           class="user-pic"
           alt="Profilbild"/>
       </v-col>
-      <v-col>
-        <p>Benutzername: User1243463</p>
-        <v-form validate-on="submit lazy" @submit.prevent="submit">
+      <v-col class="test">
+        <p>Profil bearbeiten:</p>
+        <v-form>
           <v-text-field
             v-model="Anzeigename"
-            :rules="rules"
-            label="Anzeigename"
+            label="Anzeigename Ändern"
           />
         </v-form>
-        <v-file-input label="Profilbild ändern" variant="outlined"></v-file-input>
-        <v-btn variant="outlined" class="ueberschrift">
+        <v-file-input
+          accept="image/png, image/jpeg, image/bmp"
+          placeholder="Profilbild Ändern"
+          label="Profilbild Ändern"
+        >
+          <template v-slot:prepend>
+            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+              <path d="M149.1 64.8L138.7 96H64C28.7 96 0 124.7 0 160V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H373.3L362.9 64.8C356.4 45.2 338.1 32 317.4 32H194.6c-20.7 0-39 13.2-45.5 32.8zM256 192a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"/>
+            </svg>
+          </template>
+        </v-file-input>
+        <v-btn 
+          variant="outlined"
+          class="ueberschrift"
+        >
           Änderungen Speichern
         </v-btn>
       </v-col>
@@ -40,5 +52,8 @@
   }
   .ueberschrift {
     color: #ff317b;
+  }
+  .test {
+    margin-top: 2em;
   }
 </style>
